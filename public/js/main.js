5c69f295bb2bd4b6d4f5199a9902e307894583d1
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-09-24 19:40:32 -0500

    removed unneeded code
    unneeded old code from another project

--- public/js/main.js
+++ public/js/main.js
@@ -72,26 +72,3 @@ async function markIncomplete() {
 $('.message a').click(function () {
   $('form').animate({ height: 'toggle', opacity: 'toggle' }, 'slow');
 });
-
-
-// accordian JS
-// function initAcc(elem, option){
-//   document.addEventListener('click', function (e) {
-//       if (!e.target.matches(elem+' .a-btn')) return;
-//       else{
-//           if(!e.target.parentElement.classList.contains('active')){
-//               if(option==true){
-//                   var elementList = document.querySelectorAll(elem+' .a-container');
-//                   Array.prototype.forEach.call(elementList, function (e) {
-//                       e.classList.remove('active');
-//                   });
-//               }            
-//               e.target.parentElement.classList.add('active');
-//           }else{
-//               e.target.parentElement.classList.remove('active');
-//           }
-//       }
-//   });
-// }
-
-// initAcc('.accordion', false);
