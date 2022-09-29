a3e48c743b6e8cd86f8d570a1591a2f0c42e361d
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-09-28 20:50:29 -0500

    feed routes updated
    added feed routes to the server

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
 const app = express();
+// const MongoClient = require('mongodb').MongoClient;
 const mongoose = require('mongoose');
 const passport = require('passport');
 const session = require('express-session');
@@ -11,19 +12,34 @@ const logger = require('morgan');
 const connectDB = require('./config/database');
 const mainRoutes = require('./routes/main');
 const pulseCheckRoutes = require('./routes/pulseCheck');
+const feedRoutes = require('./routes/feed');
+const { response } = require('express');
 
 require('dotenv').config({ path: './config/.env' });
+
+// let db, // shorten up variables so less typing (empty)
+//     dbConnectionStr = process.env.DB_STRING,  // look here for the environmental variable connection string
+//     dbName = 'MoD' // variable assignment
+
+// MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true }) // connect to the database using string above
+// // unifiedTopology - opt in for a new verison of MongoDB connection (stays active), better performance
+// .then(client => {  // after connecting then do: (function)
+//     console.log(`Connected to ${dbName} Database`) // let us know we connected correctly
+//     db = client.db(dbName)  // assign the db variable from line 8
+// })
 
 // Passport config
 require('./config/passport')(passport);
 
 connectDB();
 
+
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger('dev'));
+
 
 //Use forms for put / delete
 app.use(methodOverride("_method"));
@@ -47,6 +63,8 @@ app.use(fileUpload());
 
 app.use('/', mainRoutes);
 app.use('/pulseCheck', pulseCheckRoutes);
+app.use('/feed', feedRoutes);
+
 
 app.listen(process.env.PORT, () => {
   console.log('Server is running, you better catch it!');
