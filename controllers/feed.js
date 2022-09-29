75598ab582039a147ab18d402b6d3b7fa29dd540
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-09-28 20:48:19 -0500

    get call
    get call for the feed

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,10 +1,24 @@
+const PulseCheck = require('../models/PulseCheck');
 var moment = require('moment');
 
 module.exports = {
-  getFeed: (req, res) => {
-    res.render('feed.ejs', {
-      title: "Live Feed - MoD Pulse Check",
-      moment: moment,
-    });
-  },
-};
+  getFeed: async (req, res) => {
+    console.log(req.user);
+    try {
+      const feedItems = await PulseCheck.find({ userId: req.user.id});
+      console.log(feedItems);
+      res.render('feed.ejs', {
+        modFeed: feedItems, 
+        moment: moment,
+        title : 'Department Pulse Check Entry',
+        trucksInService: req.body.trucksInService,
+        complexTime: req.body.complexTime, })
+    } catch (err) {
+      console.log(err);
+    }
+  }
+}
+
+    // const feedItems = await db.collection('pulsechecks').find().toArray();
+    // const incompleteCheckPoint = await db.collection('pulsechecks').countDocuments({completed: false})
+    // response.render('feed.ejs', {modFeed: feedItems, incomplete: incompleteCheckPoint  })
