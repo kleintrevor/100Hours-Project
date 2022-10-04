89fc6b297627d1dfdb4dc79dd9f4603d1273b9e6
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-10-04 14:24:32 -0500

    updated pulseCheckItem
    updated to match across the app

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,7 +5,10 @@ module.exports = {
   getFeed: async (req, res) => {
     console.log(req.user);
     try {
-      const feedItems = await PulseCheck.find({ userId: req.user.id });
+      const feedItems = await PulseCheck.find({         
+        userId: req.user.id,
+
+      });
       console.log(`these are the feed items ${feedItems}`);
       res.render('feed.ejs', {
         feedItems: feedItems, 
@@ -15,7 +18,7 @@ module.exports = {
         departmentId: req.user.department,
         date: req.body.date,
         targetStaffingLevels: req.body.targetStaffingLevels,
-        currentStaffingLevels: req.body.pulseCheckItem,
+        currentStaffingLevels: req.body.currentStaffingLevels,
         complexTime: req.body.complexTime,
         complexNumber: req.body.complexNumber,
         trucksTarget: req.body.trucksTarget,
@@ -24,7 +27,13 @@ module.exports = {
         warehouseDelivery: req.body.warehouseDelivery,
         icer: req.body.icer,
         flightChecker: req.body.flightChecker,
-       })
+        qcp: req.body.qcp,
+        mpIds: req.body.mpIds,
+        supplyTruck: req.body.supplyTruck,
+        rampExchangeTruck: req.body.rampExchangeTruck,
+        tempIceTrailer: req.body.tempIceTrailer,
+        tempCoolers: req.body.tempCoolers, 
+      });
     } catch (err) {
       console.log(err);
     }
