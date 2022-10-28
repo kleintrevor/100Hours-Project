5fcaf66286930a926f9d243d239fbff60ce3e25a
kleintrevor <98938815+kleintrevor@users.noreply.github.com>
2022-10-27 22:18:45 -0500

    removed unneeded variables

--- controllers/feed.js
+++ controllers/feed.js
@@ -5,31 +5,13 @@ module.exports = {
   getFeed: async (req, res) => {
     console.log(req.user);
     try {
-      const feedItems = await PulseCheck.find({ });
+      const feedItems = await PulseCheck.find({});
       console.log(`these are the feed items ${feedItems}`);
       res.render('feed.ejs', {
         feedItems: feedItems, 
         moment: moment,
         title : 'Department Pulse Check Entry',
-        userId: req.user.id,
-        departmentId: req.user.department,
-        date: req.body.date,
-        targetStaffingLevels: req.body.targetStaffingLevels,
-        currentStaffingLevels: req.body.currentStaffingLevels,
-        complexTime: req.body.complexTime,
-        complexNumber: req.body.complexNumber,
-        trucksTarget: req.body.trucksTarget,
-        trucksInService: req.body.trucksInService,
-        supplyRoomDelivery: req.body.supplyRoomDelivery,
-        warehouseDelivery: req.body.warehouseDelivery,
-        icer: req.body.icer,
-        flightChecker: req.body.flightChecker,
-        qcp: req.body.qcp,
-        mpIds: req.body.mpIds,
-        supplyTruck: req.body.supplyTruck,
-        rampExchangeTruck: req.body.rampExchangeTruck,
-        tempIceTrailer: req.body.tempIceTrailer,
-        tempCoolers: req.body.tempCoolers, 
+
       });
     } catch (err) {
       console.log(err);
